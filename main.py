21fa98fc8ed16be9d3cebd769f13856118860f80
jakubholan <159123546+jakubholan@users.noreply.github.com>
2025-01-15 08:35:54 +0100

    DP1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+#Definování matice
+matrix = [[1, 2, 3],  #První řádek
+        [4, 5, 6],  #Druhý řádek
+        [7, 8, 9],] #Třetí řádek
 
+#Funkce pro vytisknutí do konzole
+print(matrix[0])
+print(matrix[1])
+print(matrix[2])
